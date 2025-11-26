536edf65116ce0f6d800e0d727888c0a4ffcbedd
Julia Kempton <kemptonjq@g.cofc.edu>
2025-11-25 20:27:05 -0500

    Fixed build error

--- src/app/checkout/confirmation/page.tsx
+++ src/app/checkout/confirmation/page.tsx
@@ -1,10 +1,12 @@
 import { cookies } from "next/headers";
 import ConfirmationClient from "./ConfirmationClient";
 
-export default function ConfirmationPage() {
-  const cookieStore = cookies();
+export default async function ConfirmationPage() {
+  const cookieStore = await cookies();
+
   const pi = cookieStore.get("last_pi")?.value ?? "";
-  const redirectStatus = cookieStore.get("last_redirect_status")?.value ?? "";
+  const redirectStatus =
+    cookieStore.get("last_redirect_status")?.value ?? "";
 
   return <ConfirmationClient pi={pi} redirectStatus={redirectStatus} />;
 }
